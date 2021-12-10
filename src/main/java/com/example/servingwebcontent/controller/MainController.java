acbc5f9b2788dc9dddb6df9800a83b01abdb1339
DmitriiRezanov <dimarezanov1@gmail.com>
2021-12-11 04:29:51 +0500

    login error

--- src/main/java/com/example/servingwebcontent/controller/MainController.java
+++ src/main/java/com/example/servingwebcontent/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.servingwebcontent;
+package com.example.servingwebcontent.controller;
 
 import com.example.servingwebcontent.domain.Message;
 import com.example.servingwebcontent.repos.MessageRepo;
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
