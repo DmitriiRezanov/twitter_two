5e81dfd6c75f6f6260ff40862d8729827157bb88
DmitriiRezanov <dimarezanov1@gmail.com>
2021-12-13 04:39:07 +0500

    add communities (error)

--- src/main/java/com/example/servingwebcontent/controller/CommunityController.java
+++ src/main/java/com/example/servingwebcontent/controller/CommunityController.java
@@ -1,10 +1,21 @@
 package com.example.servingwebcontent.controller;
 
+import com.example.servingwebcontent.domain.Message;
+import com.example.servingwebcontent.domain.User;
+import com.example.servingwebcontent.repos.MessageRepo;
 import com.example.servingwebcontent.service.CommunityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/community/")
@@ -16,6 +27,12 @@ public class CommunityController {
     public CommunityController(CommunityService communityService) {
         this.communityService = communityService;
     }
+
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @Value("${upload.path}") //ищем в application.properties переменную upload.path и вставляем ее в переменную
+    private String uploadPath;
 
     @GetMapping()
     public String getAll(Model model) {
@@ -29,10 +46,41 @@ public class CommunityController {
         return "community";
     }
 
-    @PostMapping()
-    public String create(Model model, @RequestParam String name) {
-        communityService.create(name);
-        model.addAttribute("communities", communityService.getAll());
-        return "communities";
+    //Добавление сообщения
+    @PostMapping
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model,
+            @RequestParam("file") MultipartFile file) throws IOException {
+        Message message = new Message(text, tag, user);
+
+        if ( file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()){ //если дирректории не существует, то создаем ее
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();//создаем уникальное имя файла
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename)); //загружаем файл
+
+            message.setFilename(resultFilename);
+        }
+
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "community";
     }
+
+//    @PostMapping()
+//    public String create(Model model, @RequestParam String name) {
+//        communityService.create(name);
+//        model.addAttribute("communities", communityService.getAll());
+//        return "communities";
+//    }
 }
